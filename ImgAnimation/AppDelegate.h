9ee8c70715fb698c15c43eedc7d9cd66d76c9212
LiHengJun <lihengjun@dafy.com>
2017-04-12 10:13:39 +0800

    Initial Commit

---
+++ ImgAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImgAnimation
+//
+//  Created by LHJ on 2017/4/12.
+//  Copyright © 2017年 LHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
